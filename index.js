0b6b6969a5d0f1cdaa278c8ec9dd8fdd684c0e61
Hamza Khattak <hamzeekhattak56@gmail.com>
2023-03-30 15:44:30 -0700

    Navbar compelted

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ let toShow = document.getElementById('toShow');
 let toHide = document.getElementById('toHide');
 let getLinks = document.getElementById('links');
 let section_one = document.getElementById('section_one')
-getLinks.classList.add("trans")
-
-
-
-toShow.addEventListener("click", () => {
-    getLinks.style.margin = "0px"
-   toHide.style.display = "block"
-   toShow.style.display = "none"
-})
-toHide.addEventListener("click", () => {
-    getLinks.style.marginTop = "-300px"
-   toHide.style.display = "none"
-   toShow.style.display = "block"
-})
-function None() {
-    getLinks.style.marginTop = "-300px"
+var x = window.matchMedia("(max-width: 780px)")
+if (x.matches) {
+    getLinks.classList.add('mar')
+    toShow.addEventListener("click", () => {
+        getLinks.classList.add("trans")
+        getLinks.classList.remove('mar')
+        toHide.style.display = "block"
+        toShow.style.display = "none"
+    })
+    toHide.addEventListener("click", () => {
+        getLinks.classList.add('mar')
+        toHide.style.display = "none"
+        toShow.style.display = "block"
+    })
+    function None() {
+        getLinks.classList.add('mar')
+    }
 }
+
+
+
+
+
+
+
