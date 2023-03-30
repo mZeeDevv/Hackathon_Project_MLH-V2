5174920cec2186a5ddb5ec5a89048e9433af74c4
Hamza Khattak <hamzeekhattak56@gmail.com>
2023-03-30 15:02:22 -0700

    Navbar completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let toShow = document.getElementById('toShow');
 let toHide = document.getElementById('toHide');
 let getLinks = document.getElementById('links');
-// getLinks.classList.add("trans")
+getLinks.classList.add("trans")
 
 toShow.addEventListener("click", () => {
     getLinks.style.margin = "0px"
@@ -10,7 +10,7 @@ toShow.addEventListener("click", () => {
 })
 
 toHide.addEventListener("click", () => {
-    getLinks.style.margin = "-300px"
+    getLinks.style.marginTop = "-300px"
    toHide.style.display = "none"
    toShow.style.display = "block"
 })
